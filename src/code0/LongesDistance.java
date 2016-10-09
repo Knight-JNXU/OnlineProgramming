9ef761b5d252ff54aac9112752d91624522a0bc6
Knight-JXNU <1990785022@qq.com>
2016-10-09 15:50:24 +0800

    first commit

---
+++ src/code0/LongesDistance.java
@@ -0,0 +1,26 @@
+package code0;
+
+/**
+ * Created by Knight_JXNU on 2016/10/9.
+ */
+public class LongesDistance {
+    public int getDis(int[] A, int n){
+        return findMaxDiff(A, 0, n-1, 0);
+    }
+
+    public int findMaxDiff(int[] A, int left, int right, int max){
+        if(left == right){
+            return max;
+        }else{
+            // right 为下标的最大值
+            for(int i=left; i<=right; i++){
+                for(int j=i; j<=right; j++){
+                    if(A[j]-A[i] > max){
+                        max = A[j]-A[i];
+                    }
+                }
+            }
+            return (findMaxDiff(A, left+1, right, max));
+        }
+    }
+}
