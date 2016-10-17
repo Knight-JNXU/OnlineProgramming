c4fa63c82b207e73996e5984002808a1237a7077
Knight-JXNU <1990785022@qq.com>
2016-10-17 16:52:38 +0800

    最后一个字符 我觉得是正确的，但 ac 不了

---
+++ src/code0/LastChar.java
@@ -0,0 +1,29 @@
+package code0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * Created by Knight_JXNU on 2016/10/17. Main
+ */
+public class LastChar {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int t=0; t<T; t++){
+            String inputStr = br.readLine();
+            if(inputStr.length() == 1){
+                System.out.println(inputStr);
+            }else{
+                for(int i=0; i<inputStr.length()-1; i++){
+                    if(inputStr.indexOf(inputStr.substring(i, i+1), i+1) == -1){
+                        System.out.println(inputStr.substring(i, i+1));
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
