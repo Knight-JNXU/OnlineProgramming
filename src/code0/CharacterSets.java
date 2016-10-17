3f88442c9ff1ef3c7da2eb623109b877cfc3f44b
Knight-JXNU <1990785022@qq.com>
2016-10-17 14:32:08 +0800

    我觉得是正确的，但 ac 不了

---
+++ src/code0/CharacterSets.java
@@ -0,0 +1,26 @@
+package code0;
+
+import java.util.Scanner;
+
+/**
+ * Created by Knight_JXNU on 2016/10/17.
+ * http://www.nowcoder.com/practice/784efd40ed8e465a84821c8f3970b7b5?tpId=49&tqId=29297&rp=2&ru=/ta/2016test&qru=/ta/2016test/question-ranking
+ */
+public class CharacterSets {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int letterArray[] = new int['z'-'A'+1];
+            String inputStr = sc.nextLine();
+            char chars[] = inputStr.toCharArray();
+            String target = "";
+            for(char c : chars){
+                if(letterArray[c-'A']!=1){
+                    target += c;
+                    letterArray[c-'A']=1;
+                }
+            }
+            System.out.println(target);
+        }
+    }
+}
